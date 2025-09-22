a5599db5955d0b014b89f482e41080ea1e55b871
JasonHutch <jasonr.hutchii@gmail.com>
2025-09-21 23:26:48 -0500

    updated run script

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
- streamlit run assignmentTwo.py --server.port 8000 --server.address 0.0.0.0
+#!/bin/bash
+
+# Use PORT environment variable if set, else default to 8000
+PORT=${PORT:-8000}
+
+streamlit run assignmentTwo.py --server.port $PORT --server.address 0.0.0.0
